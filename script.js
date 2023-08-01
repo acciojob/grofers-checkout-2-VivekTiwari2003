02d3fc9eda9486e1a20a504d4e610ce0a9afacbd
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-08-01 16:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.addEventListener("DOMContentLoaded", () => {
+    updateTotalPrice();
+
+    const groceryTable = document.getElementById("groceryTable");
+    const priceCells = groceryTable.querySelectorAll("[data-ns-test='itemPrice']");
+
+    priceCells.forEach((cell) => {
+        cell.addEventListener("input", updateTotalPrice);
+    });
+});
+
+function updateTotalPrice() {
+    const groceryTable = document.getElementById("groceryTable");
+    const priceCells = groceryTable.querySelectorAll("[data-ns-test='itemPrice']");
+
+    let totalPrice = 0;
+    priceCells.forEach((cell) => {
+        const price = parseFloat(cell.textContent);
+        totalPrice += isNaN(price) ? 0 : price;
+    });
+
+    const grandTotalCell = document.querySelector("[data-ns-test='grandTotal']");
+    grandTotalCell.textContent = totalPrice;
+}
